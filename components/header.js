c3206cdecd7bd5fc3bbbd6d5b9e81a1a4ed67be7
rouxmrh <mroschhaden@outlook.com>
2026-08-07 15:51:36 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,1 +1,44 @@
+export function renderHeader({
+  eyebrow = "",
+  title = "",
+  description = ""
+}) {
+  const header = document.getElementById("appHeader");
 
+  if (!header) {
+    return;
+  }
+
+  header.innerHTML = `
+    <div>
+      ${
+        eyebrow
+          ? `
+            <p class="es-eyebrow">
+              ${eyebrow}
+            </p>
+          `
+          : ""
+      }
+
+      <h1>
+        ${title}
+      </h1>
+
+      ${
+        description
+          ? `
+            <p class="es-lead">
+              ${description}
+            </p>
+          `
+          : ""
+      }
+    </div>
+
+    <div
+      id="headerUser"
+      class="es-header-user"
+    ></div>
+  `;
+}
